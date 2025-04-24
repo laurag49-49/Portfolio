2daa05e94bb80e302a08338f39722886e04f3b8a
laurag49-49 <laurag49@uic.edu>
2025-04-24 14:41:16 -0500

    Day 2 edits

--- portfolio.R
+++ portfolio.R
@@ -5,3 +5,17 @@ use_git_config(user.name= "laurag49-49", user.email="laurag49@uic.edu")
 git_sitrep()
 
 #quarto create-project --type website
+
+install.packages("reticulate")
+
+reticulate::install_python()
+
+library(tensorflow)
+
+install_tensorflow(envname = "r-tensorflow")
+
+install.packages("keras")
+
+library(keras)
+
+install_keras()
